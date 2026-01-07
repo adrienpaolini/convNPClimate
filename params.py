7fd424546e5ef5f22263a2ecd2f695515ced7a2e
Francisco Passos <1572175+franciscopassos@users.noreply.github.com>
2026-01-07 23:15:16 +0000

    Dynamically compute IN_CHANNELS after dataset preprocessing.

--- params.py
+++ params.py
@@ -23,7 +23,7 @@ class Params:
     N_BLOCKS: int = 6        # default in paper is 6
     KERNEL_SIZE: int = 5     # default in paper is 5
     LENGTH_SCALE: float = 0.1  # default in paper is 0.1
-    IN_CHANNELS: int = 5     # default in paper is 25
+    IN_CHANNELS: int = 25     # default in paper is 25; this is best computed dynamically from the data available
 
     # Training parameters
     N_EPOCHS: int = 30       # default in paper is 100
